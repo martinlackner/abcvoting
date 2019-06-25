817a42a249206c2f8e4bc119e06f6c76d139b82d
Martin Lackner <lackner@sent.at>
2019-06-25 17:48:42 +0200

    additional example

--- examples.py
+++ examples.py
@@ -3,16 +3,26 @@
 from preferences import *
 import rules_approval
 
+
 ilp=True
 try:
     import gurobipy
 except:
     ilp=False
 
+
+num_cand = 5
+profile = Profile(num_cand)
+profile.add_preferences([[0,1,2],[0,1],[0,1],[1,2],[3,4],[3,4]])
+committeesize = 3
+output = rules_approval.compute_pav(profile,committeesize,ilp=ilp)
+rules_approval.print_committees(output)
+
+print "****************************************"
+
 num_cand = 6
-prof = Profile(num_cand)
-prof.add_preferences([[0,4,5],[0],[1,4,5],[1],[2,4,5],[2],[3,4,5],[3]])
-com_size = 4
+profile = Profile(num_cand)
+profile.add_preferences([[0,4,5],[0],[1,4,5],[1],[2,4,5],[2],[3,4,5],[3]])
+committeesize = 4
 
-rules_approval.allrules(prof,com_size,ilp=ilp)
-
+rules_approval.allrules(profile,committeesize,ilp=ilp)
