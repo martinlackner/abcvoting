65b772702e019534638e96c70b61d5705b13c35f
stefanschlomoforster <55197713+stefanschlomoforster@users.noreply.github.com>
2019-09-15 19:27:12 +0200

    Added Maximin AV and Minimax AV

--- rules_approval_ilp.py
+++ rules_approval_ilp.py
@@ -1,7 +1,7 @@
 # Approval-based multiwinner rules implemented as an integer linear
 # program (ILP) with Gurobi
 
-# Author: Martin Lackner
+# Author: Martin Lackner, Stefan Forster
 
 
 from committees import enough_approved_candidates, sort_committees
@@ -81,7 +81,7 @@ def compute_thiele_methods_ilp(profile, committeesize,
     return committees
 
 
-def compute_monroe_ilp(profile, committeesize, resolute):
+def compute_monroe_ilp(profile, committeesize, resolute=False):
     enough_approved_candidates(profile, committeesize)
 
     # Monroe is only defined for unit weights
@@ -90,7 +90,7 @@ def compute_monroe_ilp(profile, committeesize, resolute):
                                   " unit weights (weight=1)")
 
     num_voters = len(profile.preferences)
-    cands = list(range(profile.num_cand))
+    cands = range(profile.num_cand)
 
     # Alternative: split voters -> generate new profile with all weights = 1
     # prof2 = Profile(profile.num_cand)
@@ -138,8 +138,6 @@ def compute_monroe_ilp(profile, committeesize, resolute):
                                 for j in range(len(profile.preferences)))
                     <= num_voters * in_committee[i])
 
-    m.update()
-
     # constraint for objective variable "satisfaction"
     m.addConstr(gb.quicksum(partition[(i, j)] *
                             (i in profile.preferences[j].approved)
@@ -183,6 +181,156 @@ def compute_monroe_ilp(profile, committeesize, resolute):
     #    print "Warning (Monroe): more than 10 committees found;",
     #    print "returning first 10"
     #    committees = committees[:10]
+
+    committees = sort_committees(committees)
+
+    return committees
+
+
+def compute_maximin_ilp(profile, committeesize, resolute=False):
+    enough_approved_candidates(profile, committeesize)
+
+    voters = profile.preferences
+    num_voters = len(voters)
+    cands = range(profile.num_cand)
+
+    m = gb.Model()
+
+    # optimization goal: variable "sum_difference"
+    max_hamdistance = m.addVar(vtype=gb.GRB.INTEGER, name="max_hamdistance")
+
+    # a list of committee members
+    in_committee = m.addVars(profile.num_cand, vtype=gb.GRB.BINARY,
+                             name="in_comm")
+    m.addConstr(gb.quicksum(in_committee[c] for c in cands)
+                == committeesize)
+
+    # the single differences between the committee and the voters
+    difference = m.addVars(profile.num_cand, num_voters, vtype=gb.GRB.INTEGER,
+                           name="diff")
+
+    for i in cands:
+        for j in range(num_voters):
+            if i in voters[j].approved:
+                # constraint for the case that the candidate is approved
+                m.addConstr(difference[i, j] == 1 - in_committee[i])
+            else:
+                # constraint for the case that the candidate isn't approved
+                m.addConstr(difference[i, j] == in_committee[i])
+
+    for j in range(num_voters):
+        # maximum hamming distance is greater of equal than any individual one
+        m.addConstr(max_hamdistance >= gb.quicksum(difference[i, j]
+                                                   for i in cands
+                                                   )
+                    )
+
+    # optimization objective
+    m.setObjective(max_hamdistance, gb.GRB.MINIMIZE)
+
+    # m.setParam('OutputFlag', False)
+
+    m.setParam('OutputFlag', False)
+
+    if resolute:
+        m.setParam('PoolSearchMode', 0)
+    else:
+        # output all optimal committees
+        m.setParam('PoolSearchMode', 2)
+        # abort after (roughly) 100 optimal solutions
+        m.setParam('PoolSolutions', 1000)
+        # ignore suboptimal committees
+        m.setParam('PoolGap', 0)
+
+    m.optimize()
+
+    if m.Status != 2:
+        print "Warning (Maximin AV): solutions may be incomplete or not optimal."
+        print "(Gurobi return code", m.Status, ")"
+
+    # extract committees from model
+    committees = []
+    if resolute:
+        committees.append([c for c in cands if in_committee[c].Xn >= 0.99])
+    else:
+        for sol in range(m.SolCount):
+            m.setParam('SolutionNumber', sol)
+            committees.append([c for c in cands if in_committee[c].Xn >= 0.99])
+
+    committees = sort_committees(committees)
+
+    return committees
+
+
+def compute_minimax_ilp(profile, committeesize, resolute=False):
+    enough_approved_candidates(profile, committeesize)
+
+    voters = profile.preferences
+    num_voters = len(voters)
+    cands = range(profile.num_cand)
+
+    m = gb.Model()
+
+    # optimization goal: variable "sum_difference"
+    sum_differences = m.addVar(vtype=gb.GRB.INTEGER, name="sum_diff")
+
+    # a list of committee members
+    in_committee = m.addVars(profile.num_cand, vtype=gb.GRB.BINARY,
+                             name="in_comm")
+    m.addConstr(gb.quicksum(in_committee[c] for c in cands)
+                == committeesize)
+
+    # the single differences between the committee and the voters
+    difference = m.addVars(profile.num_cand, num_voters, vtype=gb.GRB.INTEGER,
+                           name="diff")
+
+    for i in cands:
+        for j in range(num_voters):
+            if i in voters[j].approved:
+                # constraint for the case that the candidate is approved
+                m.addConstr(difference[i, j] == 1 - in_committee[i])
+            else:
+                # constraint for the case that the candidate isn't approved
+                m.addConstr(difference[i, j] == in_committee[i])
+
+    # sum_differences
+    m.addConstr(sum_differences == gb.quicksum(difference[i, j]
+                                               for i in cands
+                                               for j in range(num_voters)
+                                               )
+                )
+
+    # optimization objective
+    m.setObjective(sum_differences, gb.GRB.MINIMIZE)
+
+    # m.setParam('OutputFlag', False)
+
+    m.setParam('OutputFlag', False)
+
+    if resolute:
+        m.setParam('PoolSearchMode', 0)
+    else:
+        # output all optimal committees
+        m.setParam('PoolSearchMode', 2)
+        # abort after (roughly) 100 optimal solutions
+        m.setParam('PoolSolutions', 100)
+        # ignore suboptimal committees
+        m.setParam('PoolGap', 0)
+
+    m.optimize()
+
+    if m.Status != 2:
+        print "Warning (Minimax AV): solutions may be incomplete or not optimal."
+        print "(Gurobi return code", m.Status, ")"
+
+    # extract committees from model
+    committees = []
+    if resolute:
+        committees.append([c for c in cands if in_committee[c].Xn >= 0.99])
+    else:
+        for sol in range(m.SolCount):
+            m.setParam('SolutionNumber', sol)
+            committees.append([c for c in cands if in_committee[c].Xn >= 0.99])
 
     committees = sort_committees(committees)
 
