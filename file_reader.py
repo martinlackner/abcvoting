dc29f7bd2225565b4825331d9ae2ddbbc13bf6ac
benjaminkrenn <benjaminkrenn0+github@gmail.com>
2020-01-10 10:10:37 +0100

    implement file reader for soi and toi

--- file_reader.py
+++ file_reader.py
@@ -1,35 +1,129 @@
+import os
+from copy import copy
+
+script_dir = os.path.dirname(__file__)
+print script_dir
 
 
-def read_soi_file(filename, max_approval_percent=0.8):
+def load_sois_from_dir(dir_name, max_approval_percent=1.0):
+	file_dir, files = get_file_names(dir_name)
+
+	approval_profiles = []
+	all_voters = set()
+	profiles = []
+	if file_dir is not None:
+		files = sorted(files)  # sorts from oldest to newest if name is sortable by date (YYYYMMDD)
+		for f in files:
+			if f.endswith('.soi'):
+				''' # can be added if not all soi from a directory are needed.
+				if from_date is not None or to_date is not None:
+					date = f.split("_")[-1].split(".soi")[0]
+					if from_date is not None and date < from_date:
+						continue
+					if to_date is not None and date > to_date:
+						break
+				'''
+				candidate_map, profile, used_candidate_count = read_election_file(
+					os.path.join(file_dir, f), max_approval_percent)
+				profiles.append((candidate_map, profile, used_candidate_count))
+
+	return profiles
+
+
+def get_file_names(dir_name):
+	input_path = os.path.join(script_dir, dir_name)
+	files = []
+	file_dir = None
+	for (dir_path, _, filenames) in os.walk(input_path):
+		file_dir = dir_path
+		files = filenames
+		break
+	print files, file_dir, input_path, script_dir
+	return file_dir, files
+
+
+def get_vote(threshold, ranking):
+	appr_set = []
+	tied = False
+	curr_voters = ""
+	for i in range(len(ranking)):
+		if threshold > 0:
+			voter = ranking[i].strip()
+			if not tied:
+				if voter.startswith("{"):
+					tied = True
+					# count = 1
+					curr_voters = voter
+					if "}" in voter:
+						raise Exception("Single voter in {} is invalid")
+				else:
+					add_candidate(voter, appr_set)
+					threshold -= 1
+			else:
+				curr_voters += "," + voter
+				# count += 1
+				if "}" in voter:
+					count = add_candidate(curr_voters, appr_set)
+					curr_voters = ""
+					threshold -= count  # or -= 1
+					# count = 0
+				else:
+					continue
+		else:
+			break
+	return appr_set
+
+
+def add_candidate(rank, appr_set):
+	candidate = rank.strip()
+	if candidate.find("{") != -1:
+		if candidate[0] != "{" or candidate[-1] != "}":
+			raise Exception("Invalid format for tied candidates.",
+							rank)
+		candidates = candidate[1:-1].split(",")
+		for c in candidates:
+			appr_set.append(int(c.strip()))
+	else:
+		appr_set.append(int(candidate))
+
+
+def read_election_file(filename, max_approval_percent=0.8):
 	with open(filename, "r") as f:
 		line = f.readline()
 		candidate_count = int(line.strip())
 		candidate_map = {}
 		for i in range(candidate_count):
 			parts = f.readline().strip().split(",")
-			candidate_map[int(parts[0].strip())] = parts[1].strip()
+			candidate_map[int(parts[0].strip())] = \
+				",".join(parts[1:]).strip()
 
 		parts = f.readline().split(",")
-		# voter_count = int(parts[0].strip())
+		voter_count = int(parts[0].strip())
 		# voter_sum = int(parts[1].strip())
 		unique_orders = int(parts[2].strip())
 		unique_candidates = set()
 		profile = []
 		for i in range(unique_orders):
 			line = f.readline().strip()
-			count = int(line[:line.index(",")])
-			parts = line[line.index(",")+1:].strip().split(",")
+			parts = line.split(";")
+			count = int(parts[0])
+			ranking = parts[1:]
 			vote = []
-			take_cands = min(len(parts), int(len(parts)*max_approval_percent))
-			for pos in range(max(1, take_cands)):
-				vote.append(int(parts[pos].strip()))
-				unique_candidates.add(parts[pos].strip())
+			take_cands = int(len(ranking)*max_approval_percent)
+			if take_cands > 0:
+				vote = get_vote(take_cands, ranking)
+				for cand in vote:
+					unique_candidates.add(cand)
 			for _ in range(count):
-				profile.append(vote)
+				profile.append(copy(vote))
 
 		used_candidate_count = len(unique_candidates)
 
-		if len(profile) != unique_orders:
-			print("Error: Missing unique rankings")
+		if len(profile) != voter_count:
+			print("Error: Missing voters")
 
 		return candidate_map, profile, used_candidate_count
+
+
+print load_sois_from_dir("/data/")
+
