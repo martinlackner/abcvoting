a635cff7e6659414928c3d1b018f0f9ab16f5dd3
Martin Lackner <unexpected@sent.at>
2020-03-03 12:06:50 +0100

    print committees, added Hamming

--- committees.py
+++ committees.py
@@ -19,18 +19,30 @@ def enough_approved_candidates(profile, committeesize):
 
 
 # nicely print a list of committees
-def print_committees(committees, print_max=10):
+def print_committees(committees, print_max=10, names=None):
     if committees is None:
         print("Error: no committees returned")
         return
     if len(committees) == 1:
-        print(" 1 committee")
+        print(" 1 committee:")
     else:
         if len(committees) > print_max:
-            print(" ", len(committees), "committees,", end=' ')
+            print(" ", len(committees), "committees:,", end=' ')
             print("printing ", print_max, "of them")
         else:
-            print(" ", len(committees), "committees")
-    for com in sorted(map(tuple, committees[:print_max])):
-        print("    {", ", ".join(map(str, com)), "}")
+            print(" ", len(committees), "committees:")
+    if names is None:
+        for com in sorted(map(tuple, committees[:print_max])):
+            print("    {" + ", ".join(map(str, com)) + "}")
+    else:
+        for com in sorted(map(tuple, committees[:print_max])):
+            namedcom = [names[c] for c in com]
+            print("    {" + ", ".join(map(str, namedcom)) + "}")
     print()
+
+
+# Hamming distance
+def hamming(a, b):
+    diffs = ([x for x in a if x not in b] +
+             [x for x in b if x not in a])
+    return len(diffs)
