344d8674258f65646573f69d0bf1563295360b95
Martin Lackner <unexpected@sent.at>
2020-03-11 19:29:52 +0100

    simplify examples

--- examples/handling_preflib_files.py
+++ examples/handling_preflib_files.py
@@ -4,7 +4,6 @@ import sys
 import os
 sys.path.insert(0, '..')
 from abcvoting import fileio
-from abcvoting.preferences import Profile
 from abcvoting import abcrules
 from abcvoting import committees
 
@@ -25,37 +24,27 @@ committeesize = 2
 # directory of preflib.py
 currdir = os.path.dirname(os.path.abspath(__file__))
 
+profiles = fileio.load_preflib_files_from_dir(currdir + "/toi_examples/",
+                                              appr_percent=0.7)
 
-def compute(appr_sets, cand_count):
-    profile = Profile(cand_count)
-    profile.add_preferences(appr_sets)
+for profile in profiles:
     print("Computing a committee of size", committeesize, end=' ')
     print("with the Proportional Approval Voting (PAV) rule")
     print("given a", profile)
     print("Output:")
     output = abcrules.compute_pav(profile, committeesize, ilp=ilp)
     committees.print_committees(output)
-
     print("****************************************")
 
 
-profiles = fileio.load_election_files_from_dir(currdir + "/toi_examples/",
-                                               max_approval_percent=0.7)
+# Example to read a single file (parameter setsize)
+profile = fileio.read_preflib_file(
+    currdir + "/toi_examples/ex_2010.toi", setsize=1)
 
-for candidate_map, appr_sets, cand_count in profiles:
-    compute(appr_sets, cand_count)
-
-# Example to read a single file
-candidate_map, appr_sets, cand_count = \
-    fileio.read_election_file(currdir + "/toi_examples/ex_2010.toi",
-                              max_approval_percent=0.5)
-
-compute(appr_sets, cand_count)
-
-
-# Example with setsize
-_, appr_sets, cand_count = \
-    fileio.read_election_file(currdir + "/toi_examples/ex_2010.toi",
-                              setsize=1)
-
-compute(appr_sets, cand_count)
+print("Computing a committee of size", committeesize, end=' ')
+print("with Phragmen's sequential rule")
+print("given a", profile)
+print("Output:")
+output = abcrules.compute_seqphragmen(profile, committeesize)
+committees.print_committees(output)
+print("****************************************")
--- examples/random_profiles.py
+++ examples/random_profiles.py
@@ -5,7 +5,6 @@ import random
 sys.path.insert(0, '..')
 
 from abcvoting import genprofiles
-from abcvoting.preferences import Profile
 from abcvoting import abcrules
 from abcvoting import committees
 
@@ -13,86 +12,61 @@ from abcvoting import committees
 random.seed(31415)
 
 committeesize = 3
-c_count = 10
+num_cand = 10
 
 
-def compute(appr_sets, cand_count, gen_profile_name):
-    """As long as the parameters are not set to guarantee that
-    one voter votes for at least committeesize candidates
-    it can always happen that less candidates than committeesize are
-    approved.
-    For this it is recommended to use a loop and check before
-    computing the rule.
-    This method returns False in case of too few approved candidates."""
-    profile = Profile(cand_count)
-    profile.add_preferences(appr_sets)
-    try:
-        committees.enough_approved_candidates(profile, committeesize)
-    except Exception:
-        return False
-    print("Computing a committee of size", committeesize, end=' ')
-    print("with the Proportional Approval Voting (PAV) rule")
-    print("given a randomly generated profile through the method",
-          gen_profile_name)
-    print(profile)
+def compute(profile, gen_profile_name):
+    print("Computing Proportional Approval Voting (PAV)")
+    print("for a randomly generated profile via " +
+          gen_profile_name + ":")
+    print(str(profile))
     print("Output:")
     output = abcrules.compute_pav(profile, committeesize, ilp=False)
     committees.print_committees(output)
-
     print("****************************************")
-    return True
 
 
-while True:
-    appr_sets = genprofiles.random_urn_profile(c_count, 3,
-                                               2, 0.4)
-    if compute(appr_sets, c_count, "random_urn"):
-        break
-while True:
-    appr_sets = genprofiles.\
-        random_urn_party_list_profile(c_count, 3, 2, 0.4, uniform=False)
-    if compute(appr_sets, c_count, "random_urn_party_list"):
-        break
-
-
-appr_sets = genprofiles.random_IC_profile(c_count, 3, 4)
-if not compute(appr_sets, c_count, "random_IC"):
-    print("This should not be possible as setsize=4 is larger than",
-          "committeesize", committeesize)
-
-
-appr_sets = \
-    genprofiles.random_IC_party_list_profile(c_count, 3,
-                                             2, uniform=True)
-if not compute(appr_sets, c_count, "random_IC_party_list"):
-    print("This should not be possible as two parties and 10 candidates"
-          + " with uniform split equals 5 candidates per party",
-          "which is greater than the committeesize", committeesize)
-
-
-appr_sets = genprofiles.\
-    random_mallows_profile(c_count, 4, 4, 0.7)
-if not compute(appr_sets, c_count, "random_mallows"):
-    print("This should not be possible as setsize=4 is larger than",
-          "committeesize", committeesize)
+"""
+For some methods, it might happen that fewer candidates
+than committeesize are approved (in total by all voters).
+We thus recommended to verify this before computing the rule.
+"""
 
 while True:
-    appr_sets = genprofiles.\
-        random_2d_points_profile(c_count, 4, "twogroups",
-                                 "uniform_square", 0.5, 1.9)
-    if compute(appr_sets, c_count, "random_2d_points"):
+    profile = genprofiles.random_urn_profile(num_cand, 5, 2, 0.4)
+    try:
+        committees.enough_approved_candidates(profile, committeesize)
         break
+    except ValueError:
+        pass
+compute(profile, "random_urn")
 
 while True:
-    appr_sets = genprofiles. \
-        random_2d_points_party_list_profile(c_count, 4, 2, "twogroups",
-                                            "normal", 0.5,
-                                            uniform=False)
-    if compute(appr_sets, c_count, "random_2d_points_party_list"):
+    profile = genprofiles.random_urn_party_list_profile(
+        num_cand, 3, 2, 0.4, uniform=False)
+    try:
+        committees.enough_approved_candidates(profile, committeesize)
         break
+    except ValueError:
+        pass
+compute(profile, "random_urn_party_list")
+
+profile = genprofiles.random_IC_profile(num_cand, 5, 4)
+compute(profile, "random_IC")
+
+profile = genprofiles.random_IC_party_list_profile(
+    num_cand, 5, 2, uniform=True)
+compute(profile, "random_IC_party_list")
+
+profile = genprofiles.random_mallows_profile(num_cand, 4, 4, 0.7)
+compute(profile, "random_mallows")
 
 while True:
-    appr_sets = genprofiles.random_IC_party_list_profile(
-        10, 2, 3, uniform=False)
-    if compute(appr_sets, c_count, "random_IC_party_list"):
+    profile = genprofiles.random_2d_points_profile(
+        num_cand, 4, "twogroups", "uniform_square", 0.5, 1.9)
+    try:
+        committees.enough_approved_candidates(profile, committeesize)
         break
+    except ValueError:
+        pass
+compute(profile, "random_2d_points")
