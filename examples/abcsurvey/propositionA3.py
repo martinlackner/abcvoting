9f9ac49cd401d416d70c4ccfb8d2c6ada92aa930
Martin Lackner <unexpected@sent.at>
2021-11-29 12:50:14 +0100

    corrected counterexamples for proposition A.3 and A.4 (abcbook)

--- examples/abcsurvey/propositionA3.py
+++ examples/abcsurvey/propositionA3.py
@@ -7,6 +7,7 @@ by Martin Lackner and Piotr Skowron
 from abcvoting import abcrules
 from abcvoting.preferences import Profile
 from abcvoting import misc
+from collections import namedtuple
 
 
 print(misc.header("Proposition A.3", "*"))
@@ -15,47 +16,111 @@ num_cand = 8
 a, b, c, d, e, f, g, h = range(num_cand)  # a = 0, b = 1, c = 2, ...
 cand_names = "abcdefgh"
 
+MonotonicityInstance = namedtuple(
+    "MonotonicityInstance",
+    "rule_id committeesize approval_sets with_additional_voter mod_approval_set committees_first committees_after",
+)
+
 monotonicity_instances = [
-    (
-        "seqphragmen",
-        3,  # from Xavier Mora, Maria Oliver (2015)
-        [{0, 1}] * 10 + [{2}] * 3 + [{3}] * 12 + [{0, 1, 2}] * 21 + [{2, 3}] * 6,
-        True,
-        {0, 1},
-        [{0, 1, 3}],
-        [{0, 2, 3}, {1, 2, 3}],
+    MonotonicityInstance(
+        # from Xavier Mora, Maria Oliver (2015)
+        rule_id="seqphragmen",
+        committeesize=3,
+        approval_sets=[{0, 1}] * 10 + [{2}] * 3 + [{3}] * 12 + [{0, 1, 2}] * 21 + [{2, 3}] * 6,
+        with_additional_voter=True,
+        mod_approval_set={0, 1},
+        committees_first=[{0, 1, 3}],
+        committees_after=[{0, 2, 3}],
     ),
-    (
-        "seqphragmen",
-        3,  # from Xavier Mora, Maria Oliver (2015)
-        [{2}] * 7 + [{0, 1}] * 4 + [{0, 1, 2}] + [{0, 1, 3}] * 16 + [{2, 3}] * 4,
-        False,
-        {0, 1},
-        [{0, 1, 2}],
-        [{0, 2, 3}, {1, 2, 3}],
+    MonotonicityInstance(  # from Sanchez-Fernandez and Fisteus (2019)
+        rule_id="minimaxphragmen",
+        committeesize=6,
+        approval_sets=[{1, 2, 3, 4, 5}] * 13 + [{0, 6}, {0}] * 2 + [{6}] * 1,
+        with_additional_voter=True,
+        mod_approval_set={0, 1, 2, 3, 4, 5},
+        committees_first=[{0, 1, 2, 3, 4, 5}, {1, 2, 3, 4, 5, 6}],
+        committees_after=[
+            {0, 1, 2, 3, 4, 6},
+            {0, 1, 2, 3, 5, 6},
+            {0, 1, 2, 4, 5, 6},
+            {0, 1, 3, 4, 5, 6},
+            {0, 2, 3, 4, 5, 6},
+        ],
     ),
-    (
-        "rule-x",
-        3,
-        [{1, 3}, {0, 1}, {1, 3, 4}, {0, 4}, {2, 3, 4}, {2, 4}, {2, 3, 4}, {0, 2, 4}, {1, 2, 3}],
-        True,
-        [0],
-        [{0, 3, 4}],
-        [{1, 2, 4}],
+    MonotonicityInstance(
+        # from Sanchez-Fernandez and Fisteus (2019)
+        rule_id="seqpav",
+        committeesize=4,
+        approval_sets=[{a, b, d}] * 7 + [{a, b, e}] * 4 + [{a, c, d}] * 3 + [{a, c, e}] * 5,
+        with_additional_voter=True,
+        mod_approval_set={c, d},
+        committees_first=[{a, b, c, d}],
+        committees_after=[{a, b, d, e}],
     ),
-    (
-        "rule-x",
-        3,
-        [{1}, {1, 4, 5}, {1, 4}, {2}, {1, 4}, {2, 5}, {5}],
-        False,
-        {1, 4, 5},
-        [{1, 4, 5}],
-        [{1, 2, 5}, {1, 4, 5}, {2, 4, 5}],
+    MonotonicityInstance(
+        rule_id="seqcc",
+        committeesize=3,
+        approval_sets=[
+            {0},
+            {0},
+            {0},
+            {2, 3, 0},
+            {1},
+            {1, 2},
+            {1, 2},
+            {1, 3},
+            {2},
+            {2},
+            {3},
+            {3},
+        ],
+        with_additional_voter=True,
+        mod_approval_set={0, 3},
+        committees_first=[{0, 2, 3}],
+        committees_after=[{0, 1, 2}],
     ),
-    (
-        "rule-x-without-phragmen-phase",
-        4,
-        [
+    MonotonicityInstance(
+        rule_id="greedy-monroe",
+        committeesize=3,
+        approval_sets=[
+            {1, 2, 3},
+            {0, 2, 5},
+            {0, 3, 4},
+            {2, 4},
+            {0, 1},
+            {3, 5},
+            {3, 5},
+            {1, 4},
+            {1, 5},
+        ],
+        with_additional_voter=True,
+        mod_approval_set={4},
+        committees_first=[{1, 4, 5}],
+        committees_after=[{1, 2, 3}],
+    ),
+    MonotonicityInstance(
+        rule_id="rule-x",
+        committeesize=3,
+        approval_sets=[
+            {1, 3},
+            {0, 1},
+            {1, 3, 4},
+            {0, 4},
+            {2, 3, 4},
+            {2, 4},
+            {2, 3, 4},
+            {0, 2, 4},
+            {1, 2, 3},
+        ],
+        with_additional_voter=True,
+        mod_approval_set={0},
+        committees_first=[{0, 3, 4}],
+        committees_after=[{1, 2, 4}],
+    ),
+    MonotonicityInstance(
+        rule_id="rule-x-without-phragmen-phase",
+        committeesize=4,
+        approval_sets=[
             {0, 1, 2},
             {0, 6},
             {3, 4},
@@ -69,15 +134,106 @@ monotonicity_instances = [
             {3, 4, 5},
             {2, 4, 7},
         ],
-        True,
-        {4},
-        [{0, 1, 4}],
-        [{0, 1, 4}, {0, 3, 7}],
+        with_additional_voter=True,
+        mod_approval_set={4},
+        committees_first=[{0, 1, 4}],
+        committees_after=[{0, 3, 7}],
     ),
-    (
-        "revseqpav",
-        3,
-        [
+    MonotonicityInstance(
+        # from Sanchez-Fernandez and Fisteus (2019)
+        rule_id="monroe",
+        committeesize=4,
+        approval_sets=[{a, e}] * 5
+        + [{a, g}] * 4
+        + [{b, e}] * 5
+        + [{b, h}] * 4
+        + [{c, f}] * 5
+        + [{c, g}] * 4
+        + [{d, f}] * 3
+        + [{d, h}] * 3,
+        with_additional_voter=True,
+        mod_approval_set={e},
+        committees_first=[{e, f, g, h}],
+        committees_after=[{a, b, c, d}, {e, f, g, h}],
+    ),
+    MonotonicityInstance(
+        # from Sanchez-Fernandez and Fisteus (2019)
+        rule_id="pav",
+        committeesize=4,
+        approval_sets=[{0, 1}, {0, 1, 2}, {4, 5}, {4, 5}]
+        + [{0, 4}, {1, 4}, {2, 4}, {0, 5}, {1, 5}, {2, 5}, {0, 6}, {1, 6}, {2, 6}] * 3
+        + [{3}] * 100,
+        with_additional_voter=False,
+        mod_approval_set={2, 3},
+        committees_first=[{0, 1, 2, 3}],
+        committees_after=[{3, 4, 5, 6}],
+    ),
+    MonotonicityInstance(
+        # from Sanchez-Fernandez and Fisteus (2019)
+        rule_id="cc",
+        committeesize=3,
+        approval_sets=[{a}, {a, d}, {a, e}, {c, d}, {c, e}, {b}] * 2 + [{d}],
+        with_additional_voter=False,
+        mod_approval_set={b, c},
+        committees_first=[{a, b, c}],
+        committees_after=[{a, b, c}, {b, d, e}],
+    ),
+    MonotonicityInstance(
+        # from Sanchez-Fernandez and Fisteus (2019)
+        rule_id="monroe",
+        committeesize=3,
+        approval_sets=[{a}, {a, d}, {a, e}] * 2 + [{b}, {c, d}] * 4 + [{b, e}] + [{c, e}] * 3,
+        with_additional_voter=False,
+        mod_approval_set={b, c},
+        committees_first=[{a, b, c}],
+        committees_after=[{a, b, c}, {b, d, e}],
+    ),
+    MonotonicityInstance(  # from Sanchez-Fernandez and Fisteus (2019)
+        rule_id="minimaxphragmen",
+        committeesize=6,
+        approval_sets=[{7}] + [{1, 2, 3, 4, 5}] * 13 + [{0, 6}, {0}] * 2 + [{6}],
+        with_additional_voter=False,
+        mod_approval_set={0, 1, 2, 3, 4, 5},
+        committees_first=[{0, 1, 2, 3, 4, 5}, {1, 2, 3, 4, 5, 6}],
+        committees_after=[
+            {0, 1, 2, 3, 4, 6},
+            {0, 1, 2, 3, 5, 6},
+            {0, 1, 2, 4, 5, 6},
+            {0, 1, 3, 4, 5, 6},
+            {0, 2, 3, 4, 5, 6},
+        ],
+    ),
+    MonotonicityInstance(  # from Sanchez-Fernandez and Fisteus (2019)
+        rule_id="minimaxav",
+        committeesize=5,
+        approval_sets=[{5}, {1}, {2}, {3}, {1, 4, 6}, {1, 4, 7}, {1, 5, 6}, {1, 5, 7}, {1, 6}],
+        with_additional_voter=False,
+        mod_approval_set={1, 2, 3, 4},
+        committees_first=[{1, 2, 3, 4, 5}],
+        committees_after=[{1, 2, 3, 6, 7}],
+    ),
+    MonotonicityInstance(  # from Xavier Mora, Maria Oliver (2015)
+        rule_id="seqphragmen",
+        committeesize=3,
+        approval_sets=[{2}] * 7 + [{0, 1}] * 4 + [{0, 1, 2}] + [{0, 1, 3}] * 16 + [{2, 3}] * 4,
+        with_additional_voter=False,
+        mod_approval_set={0, 1},
+        committees_first=[{0, 1, 2}],
+        committees_after=[{0, 2, 3}],
+    ),
+    MonotonicityInstance(
+        rule_id="seqpav",
+        committeesize=3,
+        approval_sets=[{2, 3}, {0, 2}, {0, 3}, {0, 5}, {1, 2}, {1, 5}, {1, 5}, {2, 4}],
+        with_additional_voter=False,
+        mod_approval_set={0, 5},
+        committees_first=[{0, 2, 5}],
+        committees_after=[{0, 1, 2}],
+    ),
+    MonotonicityInstance(
+        rule_id="revseqpav",
+        committeesize=3,
+        approval_sets=[
             {0, 4},
             {1, 2, 3},
             {3, 4},
@@ -97,221 +253,123 @@ monotonicity_instances = [
             {1, 2, 3},
             {1, 2},
         ],
-        False,
-        {2, 3},
-        [{2, 3, 4}],
-        [{1, 3, 4}],
+        with_additional_voter=False,
+        mod_approval_set={2, 3},
+        committees_first=[{2, 3, 4}],
+        committees_after=[{1, 3, 4}],
     ),
-    (
-        "greedy-monroe",
-        3,
-        [{1, 2, 3}, {0, 2, 5}, {0, 3, 4}, {2, 4}, {0, 1}, {3, 5}, {3, 5}, {1, 4}, {1, 5}],
-        True,
-        {4},
-        [{1, 4, 5}],
-        [{1, 2, 3}],
-    ),
-    ("greedy-monroe", 2, [{3}, {2}, {1}, {0, 2}], False, {1, 2}, [{1, 2}], [{0, 2}]),
-    (
-        "pav",
-        4,  # from Sanchez-Fernandez and Fisteus (2019)
-        [{0, 1}, {0, 1, 2}, {4, 5}, {4, 5}]
-        + [{0, 4}, {1, 4}, {2, 4}, {0, 5}, {1, 5}, {2, 5}, {0, 6}, {1, 6}, {2, 6}] * 3
-        + [{3}] * 100,
-        False,
-        {2, 3},
-        [{0, 1, 2, 3}],
-        [{3, 4, 5, 6}],
-    ),
-    (
-        "cc",
-        3,  # from Sanchez-Fernandez and Fisteus (2019)
-        [{a}, {a, d}, {a, e}, {c, d}, {c, e}, {b}] * 2 + [{d}],
-        False,
-        {b, c},
-        [{a, b, c}],
-        [{a, b, c}, {b, d, e}],
-    ),
-    (
-        "monroe",
-        4,  # from Sanchez-Fernandez and Fisteus (2019)
-        [{a, e}] * 5
-        + [{a, g}] * 4
-        + [{b, e}] * 5
-        + [{b, h}] * 4
-        + [{c, f}] * 5
-        + [{c, g}] * 4
-        + [{d, f}] * 3
-        + [{d, h}] * 3,
-        True,
-        {e},
-        [{e, f, g, h}],
-        [{a, b, c, d}, {e, f, g, h}],
-    ),
-    (
-        "monroe",
-        3,
-        [{a}, {a, d}, {a, e}] * 2 + [{b}, {c, d}] * 4 + [{b, e}] + [{c, e}] * 3,
-        False,
-        {b, c},
-        [{a, b, c}],
-        [{a, b, c}, {b, d, e}],
-    ),
-    (
-        "seqpav",
-        3,
-        [{1, 2}, {1, 3}, {4, 5}, {0, 4}, {2, 5}, {0, 1}, {1, 5}, {0, 4}],
-        False,
-        {4, 5},
-        [{1, 4, 5}],
-        [{0, 1, 5}, {1, 4, 5}],
-    ),
-    (
-        "seqpav",
-        4,  # from Sanchez-Fernandez and Fisteus (2019)
-        [{a, b, d}] * 7 + [{a, b, e}] * 4 + [{a, c, d}] * 3 + [{a, c, e}] * 5,
-        True,
-        {c, d},
-        [{a, b, c, d}],
-        [{a, b, d, e}],
-    ),
-    (
-        "seqcc",
-        3,
-        [{0}, {0}, {0, 1, 2}, {0, 3}, {0, 3}, {1}, {1}, {1, 3}, {2}, {2}, {2}, {3}],
-        True,
-        {1, 2},
-        [{0, 1, 2}],
-        [{0, 1, 2}, {0, 2, 3}, {1, 2, 3}],
-    ),
-    (
-        "seqcc",
-        3,
-        [{4}, {0}, {0}, {0, 1, 2}, {0, 3}, {0, 3}, {1}, {1}, {1, 3}, {2}, {2}, {2}, {3}],
-        False,
-        {1, 2},
-        [{0, 1, 2}],
-        [{0, 1, 2}, {0, 2, 3}, {1, 2, 3}],
-    ),
-    (
-        "minimaxphragmen",
-        6,  # from Sanchez-Fernandez and Fisteus (2019)
-        [{1, 2, 3, 4, 5}] * 13 + [{0, 6}, {0}] * 2 + [{6}] * 1,
-        True,
-        {0, 1, 2, 3, 4, 5},
-        [{0, 1, 2, 3, 4, 5}, {1, 2, 3, 4, 5, 6}],
-        [
-            {0, 1, 2, 3, 4, 6},
-            {0, 1, 2, 3, 5, 6},
-            {0, 1, 2, 4, 5, 6},
-            {0, 1, 3, 4, 5, 6},
-            {0, 2, 3, 4, 5, 6},
+    MonotonicityInstance(
+        rule_id="seqcc",
+        committeesize=3,
+        approval_sets=[
+            {4},
+            {0},
+            {0, 3},
+            {1},
+            {1},
+            {1},
+            {0, 2},
+            {0, 2},
+            {1, 2, 3},
+            {2},
+            {2},
+            {3},
+            {3},
         ],
+        with_additional_voter=False,
+        mod_approval_set={1, 3},
+        committees_first=[{1, 2, 3}],
+        committees_after=[{0, 1, 2}],
     ),
-    (
-        "minimaxphragmen",
-        6,  # from Sanchez-Fernandez and Fisteus (2019)
-        [{7}] + [{1, 2, 3, 4, 5}] * 13 + [{0, 6}, {0}] * 2 + [{6}],
-        False,
-        {0, 1, 2, 3, 4, 5},
-        [{0, 1, 2, 3, 4, 5}, {1, 2, 3, 4, 5, 6}],
-        [
-            {0, 1, 2, 3, 4, 6},
-            {0, 1, 2, 3, 5, 6},
-            {0, 1, 2, 4, 5, 6},
-            {0, 1, 3, 4, 5, 6},
-            {0, 2, 3, 4, 5, 6},
-        ],
+    MonotonicityInstance(
+        rule_id="rule-x",
+        committeesize=3,
+        approval_sets=[{1}, {0, 1, 4}, {1, 4}, {2}, {1, 4}, {0, 2}, {0}],
+        with_additional_voter=False,
+        mod_approval_set={0, 4},
+        committees_first=[{0, 1, 4}],
+        committees_after=[{0, 1, 2}],
     ),
-    (
-        "minimaxav",
-        5,  # from Sanchez-Fernandez and Fisteus (2019)
-        [{5}, {1}, {2}, {3}, {1, 4, 6}, {1, 4, 7}, {1, 5, 6}, {1, 5, 7}, {1, 6}],
-        False,
-        {1, 2, 3, 4},
-        [{1, 2, 3, 4, 5}],
-        [{1, 2, 3, 6, 7}],
+    MonotonicityInstance(
+        rule_id="greedy-monroe",
+        committeesize=2,
+        approval_sets=[{3}, {2}, {1}, {0, 2}],
+        with_additional_voter=False,
+        mod_approval_set={1, 2},
+        committees_first=[{1, 2}],
+        committees_after=[{0, 2}],
     ),
 ]
 
 
 for inst in monotonicity_instances:
-    (
-        rule_id,
-        committeesize,
-        approval_sets,
-        addvoter,
-        new_approval_set,
-        commsfirst,
-        commsafter,
-    ) = inst
-
-    print(misc.header(abcrules.get_rule(rule_id).longname, "-"))
+    print(misc.header(abcrules.get_rule(inst.rule_id).longname, "-"))
 
     profile = Profile(num_cand, cand_names=cand_names)
-    profile.add_voters(approval_sets)
-    original_approval_set = set(approval_sets[0])
+    profile.add_voters(inst.approval_sets)
+    original_approval_set = set(inst.approval_sets[0])
     print(profile.str_compact())
 
-    # irresolute if possible
-    if False in abcrules.get_rule(rule_id).resolute_values:
-        resolute = False
-    else:
-        resolute = True
-
-    committees = abcrules.compute(rule_id, profile, committeesize, resolute=resolute)
+    committees = abcrules.compute(inst.rule_id, profile, inst.committeesize)
     print("original winning committees:\n" + misc.str_sets_of_candidates(committees, cand_names))
     # verify correctness
-    assert committees == commsfirst
+    assert (
+        committees == inst.committees_first
+    ), f"({inst.rule_id}) {committees} != {inst.committees_first}"
     some_variant = any(
-        all(cand in committee for cand in new_approval_set) for committee in commsfirst
+        all(cand in committee for cand in inst.mod_approval_set)
+        for committee in inst.committees_first
     )
     all_variant = all(
-        all(cand in committee for cand in new_approval_set) for committee in commsfirst
+        all(cand in committee for cand in inst.mod_approval_set)
+        for committee in inst.committees_first
     )
     assert some_variant or all_variant
     if all_variant:
         assert not all(
-            all(cand in committee for cand in new_approval_set) for committee in commsafter
+            all(cand in committee for cand in inst.mod_approval_set)
+            for committee in inst.committees_after
         )
     else:
         assert not any(
-            all(cand in committee for cand in new_approval_set) for committee in commsafter
+            all(cand in committee for cand in inst.mod_approval_set)
+            for committee in inst.committees_after
         )
 
-    if addvoter:
-        print("additional voter: " + misc.str_set_of_candidates(new_approval_set, cand_names))
-        approval_sets.append(new_approval_set)
+    if inst.with_additional_voter:
+        print("additional voter: " + misc.str_set_of_candidates(inst.mod_approval_set, cand_names))
+        new_approval_set = inst.mod_approval_set
+        profile.add_voter(new_approval_set)
     else:
-        approval_sets[0] = list(set(new_approval_set) | set(approval_sets[0]))
+        new_approval_set = list(set(inst.mod_approval_set) | set(inst.approval_sets[0]))
         print(
             "change of voter 0: "
             + misc.str_set_of_candidates(list(original_approval_set), cand_names)
             + " --> "
-            + misc.str_set_of_candidates(approval_sets[0], cand_names)
+            + misc.str_set_of_candidates(new_approval_set, cand_names)
         )
+        profile[0] = new_approval_set
 
-    profile = Profile(num_cand, cand_names=cand_names)
-    profile.add_voters(approval_sets)
-
-    committees = abcrules.compute(rule_id, profile, committeesize, resolute=resolute)
+    committees = abcrules.compute(inst.rule_id, profile, inst.committeesize)
     print(
         "\nwinning committees after the modification:\n"
         + misc.str_sets_of_candidates(committees, cand_names)
     )
 
     # verify correctness
-    assert committees == commsafter
+    assert (
+        committees == inst.committees_after
+    ), f"({inst.rule_id}) {committees} != {inst.committees_after}"
 
-    print(f"{abcrules.get_rule(rule_id).shortname} fails ", end="")
-    if addvoter:
-        if len(new_approval_set) == 1:
+    print(f"{abcrules.get_rule(inst.rule_id).shortname} fails ", end="")
+    if inst.with_additional_voter:
+        if len(inst.mod_approval_set) == 1:
             print("candidate", end="")
         else:
             print("support", end="")
         print(" monotonicity with additional voters")
     else:
-        if len(set(new_approval_set) - original_approval_set) == 1:
+        if len(inst.mod_approval_set) == 1:
             print("candidate", end="")
         else:
             print("support", end="")
--- examples/abcsurvey/propositionA4.py
+++ examples/abcsurvey/propositionA4.py
@@ -7,6 +7,7 @@ by Martin Lackner and Piotr Skowron
 from abcvoting import abcrules
 from abcvoting.preferences import Profile
 from abcvoting import misc
+from collections import namedtuple
 
 
 print(misc.header("Proposition A.4", "*"))
@@ -15,91 +16,40 @@ num_cand = 6
 a, b, c, d, e, f = range(6)  # a = 0, b = 1, c = 2, ...
 cand_names = "abcdef"
 
+ManipulationInstance = namedtuple(
+    "ManipulationInstance",
+    "rule_id committeesize approval_sets manipulated_vote committees_first committees_after",
+)
+
 manipulations = [
-    ("cc", True, 2, [{a, b}] + [{a}] * 3 + [{c}], {b}, [{a, c}], [{a, b}]),
-    ("sav", False, 1, [{a, b, c}, {d, e}], {a}, [{d}, {e}], [{a}]),
-    (
-        "revseqpav",
-        False,
-        2,
-        [{a, b, c}, {b, d}, {c, b}, {a, d, e}, {b, e}],
-        {a},
-        [{b, d}, {b, e}],
-        [{a, b}],
+    ManipulationInstance(
+        rule_id="cc",
+        committeesize=2,
+        approval_sets=[{a, b}] + [{a}] * 3 + [{c}],
+        manipulated_vote={b},
+        committees_first=[{a, c}],
+        committees_after=[{a, b}],
     ),
-    (
-        "seqphragmen",
-        False,
-        2,
-        [{0, 1, 2}, {0, 1}, {1, 5}, {2, 4}, {1, 4, 5}, {1, 3, 5}],
-        {2},
-        [{1, 5}],
-        [{1, 2}],
+    ManipulationInstance(
+        rule_id="pav",
+        committeesize=3,
+        approval_sets=[{2, 3, 4}, {0, 1}, {1, 5}, {0, 2, 3}, {1, 2, 5}, {2, 4, 5}],
+        manipulated_vote={4},
+        committees_first=[{1, 2, 5}],
+        committees_after=[{1, 2, 4}],
     ),
-    (
-        "seqpav",
-        False,
-        3,
-        [{0, 1}, {1, 3}, {2, 5}, {0, 1, 5}, {1, 5}, {1, 2}],
-        {0},
-        [{1, 2, 5}],
-        [{0, 1, 5}],
+    ManipulationInstance(
+        rule_id="seqpav",
+        committeesize=3,
+        approval_sets=[{0, 1}, {1, 3}, {2, 5}, {0, 1, 5}, {1, 5}, {1, 2}],
+        manipulated_vote={0},
+        committees_first=[{1, 2, 5}],
+        committees_after=[{0, 1, 5}],
     ),
-    (
-        "minimaxav",
-        False,
-        3,
-        [{0, 1, 2}, {1, 3}, {0, 1, 4}, {0, 1, 3}, {0, 1, 4}, {0, 1}],
-        {2},
-        [{0, 1, 3}],
-        [{0, 1, 2}],
-    ),
-    (
-        "pav",
-        False,
-        3,
-        [{2, 3, 4}, {0, 1}, {1, 5}, {0, 2, 3}, {1, 2, 5}, {2, 4, 5}],
-        {4},
-        [{1, 2, 5}],
-        [{1, 2, 4}],
-    ),
-    (
-        "rule-x",
-        False,
-        3,
-        [{1, 2, 3}, {0, 1}, {1, 3}, {2, 3}, {3, 4}, {3, 4}],
-        {2},
-        [{1, 3, 4}],
-        [{1, 2, 3}],
-    ),
-    ("greedy-monroe", True, 2, [{0, 1}, {0, 2, 5}, {0, 2, 3}, {4, 5}], {1}, [{0, 2}], [{0, 1}]),
-    (
-        "monroe",
-        False,
-        3,
-        [
-            {1, 3},
-            {0, 1, 2},
-            {1, 4},
-            {3, 4},
-            {4, 5},
-            {1, 2, 4},
-            {2, 3, 4},
-            {1, 2},
-            {0, 5},
-            {1, 2, 3},
-            {0, 5},
-            {0, 3},
-        ],
-        {5},
-        [{0, 1, 4}],
-        [{1, 3, 5}],
-    ),
-    (
-        "seqcc",
-        False,
-        3,
-        [
+    ManipulationInstance(
+        rule_id="seqcc",
+        committeesize=3,
+        approval_sets=[
             {1, 4, 5},
             {0, 1},
             {3, 4, 5},
@@ -113,50 +63,120 @@ manipulations = [
             {0, 4, 5},
             {1, 2, 3},
         ],
-        {2},
-        [{0, 1, 3}],
-        [{1, 2, 4}],
+        manipulated_vote={2},
+        committees_first=[{0, 1, 3}],
+        committees_after=[{1, 2, 4}],
     ),
-    # ("minimaxphragmen", True, 3, [{a, b}] + [{b, c, d}] * 3, [a}, {{b, c, d}}, {{a, b, c}]),
+    ManipulationInstance(
+        rule_id="revseqpav",
+        committeesize=2,
+        approval_sets=[{a, b, c}, {b, d}, {c, b}, {a, d, e}, {b, e}],
+        manipulated_vote={a},
+        committees_first=[{b, d}],
+        committees_after=[{a, b}],
+    ),
+    ManipulationInstance(
+        rule_id="monroe",
+        committeesize=3,
+        approval_sets=[
+            {1, 3},
+            {0, 1, 2},
+            {1, 4},
+            {3, 4},
+            {4, 5},
+            {1, 2, 4},
+            {2, 3, 4},
+            {1, 2},
+            {0, 5},
+            {1, 2, 3},
+            {0, 5},
+            {0, 3},
+        ],
+        manipulated_vote={5},
+        committees_first=[{0, 1, 4}],
+        committees_after=[{1, 3, 5}],
+    ),
+    ManipulationInstance(
+        rule_id="greedy-monroe",
+        committeesize=2,
+        approval_sets=[{0, 1}, {0, 2, 5}, {0, 2, 3}, {4, 5}],
+        manipulated_vote={1},
+        committees_first=[{0, 2}],
+        committees_after=[{0, 1}],
+    ),
+    ManipulationInstance(
+        rule_id="seqphragmen",
+        committeesize=2,
+        approval_sets=[{0, 1, 2}, {0, 1}, {1, 5}, {2, 4}, {1, 4, 5}, {1, 3, 5}],
+        manipulated_vote={2},
+        committees_first=[{1, 5}],
+        committees_after=[{1, 2}],
+    ),
+    ManipulationInstance(
+        rule_id="rule-x",
+        committeesize=3,
+        approval_sets=[{1, 2, 3}, {0, 1}, {1, 3}, {2, 3}, {3, 4}, {3, 4}],
+        manipulated_vote={2},
+        committees_first=[{1, 3, 4}],
+        committees_after=[{1, 2, 3}],
+    ),
+    ManipulationInstance(
+        rule_id="minimaxav",
+        committeesize=3,
+        approval_sets=[{0, 1, 2}, {1, 3}, {0, 1, 4}, {0, 1, 3}, {0, 1, 4}, {0, 1}],
+        manipulated_vote={2},
+        committees_first=[{0, 1, 3}],
+        committees_after=[{0, 1, 2}],
+    ),
+    ManipulationInstance(
+        rule_id="sav",
+        committeesize=1,
+        approval_sets=[{a, b, c}, {d, e}],
+        manipulated_vote={a},
+        committees_first=[{d}],
+        committees_after=[{a}],
+    ),
+    # ManipulationInstance("minimaxphragmen", 3,
+    # [{a, b}] + [{b, c, d}] * 3, [a}, {{b, c, d}}, {{a, b, c}]),
     # this does not work because minimax-Phragmen does not support a specified tiebreaking
     # between committees ([a, b, c] should be prefered to [b, c, d])
 ]
 
-for manip in manipulations:
-    (rule_id, resolute, committeesize, approval_sets, modvote, commsfirst, commsafter) = manip
-
-    print(misc.header(abcrules.get_rule(rule_id).longname, "-"))
+for inst in manipulations:
+    print(misc.header(abcrules.get_rule(inst.rule_id).longname, "-"))
 
     profile = Profile(num_cand, cand_names=cand_names)
-    profile.add_voters(approval_sets)
+    profile.add_voters(inst.approval_sets)
     truepref = profile[0].approved
     print(profile.str_compact())
 
-    committees = abcrules.compute(rule_id, profile, committeesize, resolute=resolute)
+    committees = abcrules.compute(inst.rule_id, profile, inst.committeesize, resolute=True)
     print("original winning committees:\n" + misc.str_sets_of_candidates(committees, cand_names))
 
     # verify correctness
-    assert committees == commsfirst
+    assert (
+        committees == inst.committees_first
+    ), f"({inst.rule_id}) {committees} != {inst.committees_first}"
 
     print(
         "Manipulation by voter 0: "
-        + misc.str_set_of_candidates(approval_sets[0], cand_names)
+        + misc.str_set_of_candidates(inst.approval_sets[0], cand_names)
         + " --> "
-        + misc.str_set_of_candidates(modvote, cand_names)
+        + misc.str_set_of_candidates(inst.manipulated_vote, cand_names)
     )
-    if not all(cand in truepref for cand in modvote):
+    if not all(cand in truepref for cand in inst.manipulated_vote):
         print(" (not a subset!)")
 
-    approval_sets[0] = modvote
+    inst.approval_sets[0] = inst.manipulated_vote
     profile = Profile(num_cand, cand_names=cand_names)
-    profile.add_voters(approval_sets)
+    profile.add_voters(inst.approval_sets)
 
-    if resolute and rule_id == "cc":
+    if "brute-force" in abcrules.get_rule(inst.rule_id).available_algorithms:
         algorithm = "brute-force"  # correct tie-breaking between candidates
     else:
         algorithm = "fastest"
     committees = abcrules.compute(
-        rule_id, profile, committeesize, resolute=resolute, algorithm=algorithm
+        inst.rule_id, profile, inst.committeesize, resolute=True, algorithm=algorithm
     )
     print(
         "\nwinning committees after manipulation:\n"
@@ -164,12 +184,14 @@ for manip in manipulations:
     )
 
     # verify correctness
-    assert committees == commsafter, f"{committees} != {commsafter}"
+    assert (
+        committees == inst.committees_after
+    ), f"({inst.rule_id}) {committees} != {inst.committees_after}"
 
     # verify that this is a counterexample to inclusion-strategyproofness
     # with the Kelly (cautious) set extension
-    for commfirst in commsfirst:
-        for commafter in commsafter:
+    for commfirst in inst.committees_first:
+        for commafter in inst.committees_after:
             for cand in set(commfirst) & set(truepref):
                 assert cand in commafter
             assert set(commfirst) & set(truepref) < set(commafter) & set(truepref)
