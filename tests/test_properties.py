e1eee7d20e731f0137b70fa143ed91c336111df9
elvic96 <51164717+elvic96@users.noreply.github.com>
2021-12-17 10:41:48 +0100

    creating file test_properties.py

---
+++ tests/test_properties.py
@@ -0,0 +1,14 @@
+"""
+Unit tests for: properties.py
+"""
+
+import pytest
+import os
+import re
+import random
+from abcvoting.abcrules_cvxpy import cvxpy_thiele_methods
+from abcvoting.abcrules_gurobi import _gurobi_thiele_methods
+from abcvoting.output import VERBOSITY_TO_NAME, WARNING, INFO, DETAILS, DEBUG, output
+from abcvoting.preferences import Profile, Voter
+from abcvoting import abcrules, misc, scores, fileio
+from itertools import combinations
