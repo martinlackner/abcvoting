e130856a567307d233bbed929da6afc6fa6ffb7d
elvic96 <51164717+elvic96@users.noreply.github.com>
2022-01-31 07:03:07 +0100

    commiting initial test_properties.py file

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -11,4 +11,7 @@ from abcvoting.abcrules_gurobi import _gurobi_thiele_methods
 from abcvoting.output import VERBOSITY_TO_NAME, WARNING, INFO, DETAILS, DEBUG, output
 from abcvoting.preferences import Profile, Voter
 from abcvoting import abcrules, misc, scores, fileio
-from itertools import combinations
+
+# have the input committees here in a list, for different properties
+
+# then create parametrized tests. these will have say 2 different inputs
