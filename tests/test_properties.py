63cae70608f3dc25cb95cf7d0c2cb1a771ac5c72
elvic96 <51164717+elvic96@users.noreply.github.com>
2022-02-07 21:28:20 +0100

    parametrized test method, other improvements, black formatted

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -10,26 +10,16 @@ from abcvoting.output import VERBOSITY_TO_NAME, WARNING, INFO, DETAILS, DEBUG, o
 from abcvoting.preferences import Profile, Voter
 from abcvoting import abcrules, properties, misc, scores, fileio
 
-# Test from literature: Lackner and Skowron 2021
+# Test from literature: Lackner and Skowron 2020
 # With given input profile, committee returned by Monroe Rule
 # is not Pareto optimal
-def test_pareto_optimality_methods():
+@pytest.mark.parametrize("algorithm", ["brute-force", "gurobi"])
+def test_pareto_optimality_methods(algorithm):
     # profile with 4 candidates: a, b, c, d
     profile = Profile(4)
 
     # add voters in the profile
-    profile.add_voter(Voter([0]))
-    profile.add_voter(Voter([0]))
-
-    profile.add_voter(Voter([0, 2]))
-
-    profile.add_voter(Voter([0, 3]))
-
-    for i in range(10):
-        profile.add_voter(Voter([1, 2]))
-
-    for i in range(10):
-        profile.add_voter(Voter([1, 3]))
+    profile.add_voters([[0]] * 2 + [[0, 2]] + [[0, 3]] + [[1, 2]] * 10 + [[1, 3]] * 10)
 
     # compute output committee from Monroe's Rule
     monroe_output = abcrules.compute_monroe(profile, 2)
@@ -37,6 +27,9 @@ def test_pareto_optimality_methods():
     # Monroe's Rule should output winning committee {2, 3} for this input
     # It is not Pareto optimal because it is dominated by committee {0, 1}
     # Check using the methods
-    is_pareto_optimal = properties.check_pareto_optimality(profile, monroe_output[0])
+    is_pareto_optimal = properties.check_pareto_optimality(
+        profile, monroe_output[0], algorithm=algorithm
+    )
 
-    assert is_pareto_optimal == False
+    assert not is_pareto_optimal
+    assert monroe_output == [{2, 3}]
